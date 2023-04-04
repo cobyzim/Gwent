850634ab2aa022b5e1ec168007ed86407ce8a9f3
cobyzim <cobyzimmerman.home@gmail.com>
2023-04-04 14:34:17 -0600

    First Commit

---
+++ gwent/src/main/java/com/personal/gwent/GwentApplication.java
@@ -0,0 +1,13 @@
+package com.personal.gwent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwentApplication.class, args);
+	}
+
+}
---
+++ gwent/src/test/java/com/personal/gwent/GwentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.gwent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
